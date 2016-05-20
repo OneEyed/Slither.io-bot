11d76f95cb83adafaa698a0961ec65d1b2c86739
OneEyed <joelruiz2@gmail.com>
2016-05-20 15:37:33 -0500

    Updated version number

--- bot.meta.js
+++ bot.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io-bot
 // @namespace    http://slither.io/
-// @version      0.9.6
+// @version      0.9.7
 // @description  Slither.io bot
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
--- bot.meta.local.user.js
+++ bot.meta.local.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io-bot A* local
 // @namespace    http://slither.io/
-// @version      0.9.3
+// @version      0.9.7
 // @description  Slither.io-bot A* local
 // @author       Ermiya Eskandary & Théophile Cailliau & Joel Ruiz
 // @match        http://slither.io/
